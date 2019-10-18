f53d06af5da5b91534468ddd4dbf8758eb371ef1
akarelov <karelov.artyom@gmail.com>
2019-10-18 22:41:54 +0400

    add userService, RegistrationService and RegistrationController

---
+++ src/main/java/ru/sstu/library/service/UserService.java
@@ -0,0 +1,19 @@
+package ru.sstu.library.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.sstu.library.repos.UserRepo;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return userRepo.findByLogin(s);
+    }
+}
