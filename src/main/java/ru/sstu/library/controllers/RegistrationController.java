43fc0e917e0e519de89c16d902160cbd059ebd0f
Nikita054 <42584628+Nikita054@users.noreply.github.com>
2019-10-26 08:17:33 +0400

    Develop (#3)
    * first steps, add entities
    
    * Add Library service and Library controller
    
    * add userService, RegistrationService and RegistrationController
    
    * add h2 database
    
    * add security config

---
+++ src/main/java/ru/sstu/library/controllers/RegistrationController.java
@@ -0,0 +1,31 @@
+package ru.sstu.library.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.sstu.library.entities.User;
+import ru.sstu.library.service.RegistrationService;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private RegistrationService registrationService;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @PostMapping("/registration")
+    public String addUser(User user, Model model) {
+        User userFromDb = registrationService.getUserByLogin(user.getLogin());
+        if (userFromDb != null) {
+            model.addAttribute("errorRegistration", "Логин уже занят!");
+            model.addAttribute("user", user);
+            return "index";
+        }
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole(registrationService.getRoleByName("USER"));
+        registrationService.saveUser(user);
+        return "index";
+    }
+}
